5110db4f20b3deb8556b51753954d59bbe3d36b5
derekbt96 <derekbt@yahoo.com>
2024-09-16 20:10:22 -0400

    Working mostly

--- iss_simulator.py
+++ iss_simulator.py
@@ -1,73 +1,129 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-alt = (6375 + 432) * 1000 # m
-inclination = 51.6 # deg
-# speed = 7651 # m/s
-# G = 6.6743*(10**(-11))
-# M = 5.9722*(10**24)
-GM = 398602544600000.0
-r = alt * 1000
-# v = np.sqrt(GM/r)
-v = 7652.303002166208
-# T = 2*np.pi*np.sqrt((r**3)/(GM))
-T = 5589.1203437532795
 
-def quaternion_mult(q,r):
-    return [r[0]*q[0]-r[1]*q[1]-r[2]*q[2]-r[3]*q[3],
-            r[0]*q[1]+r[1]*q[0]-r[2]*q[3]+r[3]*q[2],
-            r[0]*q[2]+r[1]*q[3]+r[2]*q[0]-r[3]*q[1],
-            r[0]*q[3]-r[1]*q[2]+r[2]*q[1]+r[3]*q[0]]
 
-def point_rotation_by_quaternion(r,q):
-    q_conj = [q[0],-1*q[1],-1*q[2],-1*q[3]]
-    return quaternion_mult(quaternion_mult(q,r),q_conj)
 
-def toLatLon(v):
-    lat = np.arcsin(v[2])*180.0/np.pi
-    lon = np.arctan2(v[1],v[0])*180.0/np.pi
-    return [lat,lon]  
+class orbitTracker():
+    GM = 398602544600000.0
+    
+    def __init__(self,lat=0,lon=0,vNeg = False):
+        alt = (6375 + 432) * 1000 # m
+        self.inclination = 51.6 * np.pi/180.0
+        self.r = alt * 1000
+        # v = np.sqrt(GM/r)
+        self.speed = 7652.303002166208
+        # T = 2*np.pi*np.sqrt((r**3)/(GM))
+        self.T = 5589.1203437532795
 
-def propagate(lat,lon,tim,velNeg):
-    inclination = 51.6 * np.pi /  180.0 # deg
-    rot = np.arcsin(max(-1,min((np.pi/180.0)*lat/inclination,1))) + np.pi*velNeg
-    theta_prop = 2*np.pi*tim/86400
-    # print(theta_prop)
-    v_rot = np.array([np.sin(rot),
-                      np.cos(rot)*np.sin(inclination),
-                      np.cos(inclination)])
-    # print(v_rot)
-    q_rot = np.array([np.cos(theta_prop/2),
-                      np.sin(theta_prop/2)*v_rot[0],
-                      np.sin(theta_prop/2)*v_rot[1],
-                      np.sin(theta_prop/2)*v_rot[2]])
-    # print(q_rot)
+        self.timestamp = 0
+        
+        self.lat = lat
+        self.lon = lon
+        self.v = self.speed*np.array([np.cos(self.inclination),0,np.sin(self.inclination)])
+        if vNeg:
+            self.v[2] = -self.v[2]
 
-    vecsq = np.array([0,
-                      np.cos((np.pi/180.0)*lat),
-                      0,
-                      np.sin((np.pi/180.0)*lat)])
-    # print(vecsq)
+    def calibrate(self,lat,lon,timestamp,velPos=True):
+        self.lat = lat 
+        self.lon = lon
+        self.timestamp = timestamp
+        if not velPos:
+            self.v[2] = -1
 
-    vecsq_rotated = point_rotation_by_quaternion(vecsq, q_rot)[1:]
-    # print(vecsq_rotated)
 
-    return toLatLon(vecsq_rotated)
- 
+    def update(self,w,a,dt):
+        self.prediction(dt)
+        self.correction(w,a,dt)
+
+    def quaternion_mult(self,q,r):
+        return [r[0]*q[0]-r[1]*q[1]-r[2]*q[2]-r[3]*q[3],
+                r[0]*q[1]+r[1]*q[0]-r[2]*q[3]+r[3]*q[2],
+                r[0]*q[2]+r[1]*q[3]+r[2]*q[0]-r[3]*q[1],
+                r[0]*q[3]-r[1]*q[2]+r[2]*q[1]+r[3]*q[0]]
+
+    def point_rotation_by_quaternion(self,r,q):
+        q_conj = [q[0],-1*q[1],-1*q[2],-1*q[3]]
+        return self.quaternion_mult(self.quaternion_mult(q,r),q_conj)
+
+    def toLatLon(self,v):
+        lat = np.arcsin(v[2])*180.0/np.pi
+        lon = np.arctan2(v[1],v[0])*180.0/np.pi
+        return [lat,lon]
+
+    def propagate(self,tim):
+
+        rot = np.arcsin(max(-1,min(1, (np.pi/180.0)*self.lat/self.inclination )))
+        # print("rot: ",rot)
+
+
+        theta_prop = 2*np.pi*tim/self.T
+        if self.v[2] > 0: 
+            v_rot = np.array([np.sin(-rot),
+                            np.cos(-rot)*-np.sin(self.inclination),
+                            np.cos(self.inclination)])
+        else:
+            v_rot = np.array([np.sin(-rot),
+                            -np.cos(-rot)*-np.sin(self.inclination),
+                            np.cos(self.inclination)])
+        # print("v_rot: ",v_rot)
+        
+        q_rot = np.array([np.cos(theta_prop/2),
+                        np.sin(theta_prop/2)*v_rot[0],
+                        np.sin(theta_prop/2)*v_rot[1],
+                        np.sin(theta_prop/2)*v_rot[2]])
+       
+        pos = np.array([0,
+                        np.cos((np.pi/180.0)*self.lat),
+                        0,
+                        np.sin((np.pi/180.0)*self.lat)])
+        pos_rotated = self.point_rotation_by_quaternion(pos, q_rot)[1:]
+        # print(pos_rotated)
+
+        vel = np.append(0,np.cross(v_rot,pos[1:]))
+        vel = vel*np.linalg.norm(vel)*self.speed
+        self.v = self.point_rotation_by_quaternion(vel, q_rot)[1:]
+        # print(self.v)
+
+        location_new = self.toLatLon(pos_rotated)
+        
+        location_new[1] += self.lon
+
+        location_new[1] -= 360*tim/86400
+
+        if location_new[1] > 180:
+            location_new[1] -= 360
+        elif location_new[1] < -180:
+            location_new[1] += 360
+
+        self.lat = location_new[0]
+        self.lon = location_new[1]
+        self.timestamp += tim
+
+
+ISS = orbitTracker()
+ISS.calibrate(7.9938,64.4501,1726524904,False)
+
+lat_data = np.array([ISS.lat])
+lon_data = np.array([ISS.lon])
+tim_data = np.array([ISS.timestamp])
+
+for k in range(6):
+    ISS.propagate(600)
+    lat_data = np.append(lat_data,ISS.lat)
+    lon_data = np.append(lon_data,ISS.lon)
+    tim_data = np.append(tim_data,ISS.timestamp)
     
 
-lat = 0.0
-lon = 0.0
-lat_data = np.array([lat])
-lon_data = np.array([lon])
+log_file = 'ISS_Tracker/iss_positions_1hour.txt'
+dat = np.genfromtxt(log_file,delimiter=',')
+lat_record = dat[:,3]
+lon_record = dat[:,4]
 
-for k in range(30):
-    [lat,lon] = propagate(lat,lon,3600,1)
-    lat_data = np.append(lat_data,lat)
-    lon_data = np.append(lon_data,lon)
+plt.plot(lon_record, lat_record)
+plt.scatter(lon_record, lat_record)
 
-print(lat_data)
-print(lon_data)
+
 plt.plot(lon_data, lat_data)
 plt.scatter(lon_data, lat_data)
 
--- iss_tracker.py
+++ iss_tracker.py
@@ -7,42 +7,45 @@ import numpy as np
 
 now = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 
-if 1:
-    url = "http://api.open-notify.org/iss-now.json"
-    response = urllib.request.urlopen(url)
-    result = json.loads(response.read())
-    
-    # Extract the ISS location
-    location = result["iss_position"]
-    latstr = location['latitude']
-    lonstr = location['longitude']
-    timestamp = result['timestamp']
-else:
-    latstr = '12.34'
-    lonstr = '56.78'
-    timestamp = 000
+for i in range(30):
+    if 1:
+        url = "http://api.open-notify.org/iss-now.json"
+        response = urllib.request.urlopen(url)
+        result = json.loads(response.read())
+        
+        # Extract the ISS location
+        location = result["iss_position"]
+        latstr = location['latitude']
+        lonstr = location['longitude']
+        timestamp = result['timestamp']
+    else:
+        latstr = '12.34'
+        lonstr = '56.78'
+        timestamp = 000
 
-# Output lon and lat to the terminal in the 
-# float format
-lat = float(latstr)
-lon = float(lonstr)
-print("Latitude: " + str(lat))
-print("Longitude: " + str(lon))
+    # Output lon and lat to the terminal in the 
+    # float format
+    lat = float(latstr)
+    lon = float(lonstr)
+    print("Latitude: " + str(lat))
+    print("Longitude: " + str(lon))
 
 
 
 
-file = open('ISS_Tracker/iss_positions.txt','a+')
-file.write(now)
-file.write(',')
-file.write(str(time.time()))
-file.write(',')
-file.write(str(timestamp))
-file.write(',')
-file.write(latstr)
-file.write(',')
-file.write(lonstr)
-file.write('\n')
+    file = open('ISS_Tracker/iss_positions_2hour.txt','a+')
+    file.write(now)
+    file.write(',')
+    file.write(str(time.time()))
+    file.write(',')
+    file.write(str(timestamp))
+    file.write(',')
+    file.write(latstr)
+    file.write(',')
+    file.write(lonstr)
+    file.write('\n')
 
-file.close()
+    file.close()
+    print("waiting 10 minutes, loop: ",i)
+    time.sleep(5*60)
 
